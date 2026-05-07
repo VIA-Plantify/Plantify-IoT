f07b1711175f7508029d42211d3d563dc0608995
Mihai Rotaru <355401@via.dk>
2026-05-07 15:51:37 +0200

    Separated mqqt from main.c into it's separate driver ( mqtt.c & mqtt.h ), implemented the better pump control into the pump.c driver. Cleaned up main.c

--- ArduinoProject/lib/drivers/pump.c
+++ ArduinoProject/lib/drivers/pump.c
@@ -1,3 +1,4 @@
+#include "mqtt.h"
 #include "pump.h"
 #include <util/delay.h>
 #include <avr/io.h>
@@ -26,9 +27,12 @@ bool pump_is_running(void)
 void pump_run_for(uint32_t miliseconds)
 {
     pump_on();
-    for(uint32_t i = 0; i < miliseconds; i++)
+    uint32_t elapsed = 0;
+    while (elapsed < milliseconds)
     {
-        _delay_ms(1);
+        _delay_ms(100);
+        elapsed += 100;
+        mqtt_handle_incoming();
     }
     pump_off();
 }
