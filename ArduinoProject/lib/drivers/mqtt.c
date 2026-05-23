9c13095a41dbdfd165057a603885c0d163121c10
Mihai Rotaru <355401@via.dk>
2026-05-23 18:50:40 +0200

    removed buzzer_beep() from mqtt.c

--- ArduinoProject/lib/drivers/mqtt.c
+++ ArduinoProject/lib/drivers/mqtt.c
@@ -389,8 +389,6 @@ void mqtt_handle_incoming(void)
                 led_on(1);
             else if (strcmp(payload_str, "led_off") == 0)
                 led_off(1);
-            else if (strcmp(payload_str, "beep") == 0)
-                buzzer_beep();
             else if (strncmp(payload_str, "pump_on_", 8) == 0)
                 pump_run_for((uint8_t)atoi(&payload_str[8]) * 1000);
             else
